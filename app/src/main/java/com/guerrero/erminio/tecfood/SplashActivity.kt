cd166f0ce9951751c16404723da72e3d1f3d9ebe
PROGRAMMINGEGP <guerreroerminio30@gamil.com>
2024-06-01 22:12:17 -0500

    build of Splash

---
+++ app/src/main/java/com/guerrero/erminio/tecfood/SplashActivity.kt
@@ -0,0 +1,36 @@
+package com.guerrero.erminio.tecfood
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Looper
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.guerrero.erminio.tecfood.databinding.ActivitySplashBinding
+import com.rommansabbir.animationx.Attention
+import com.rommansabbir.animationx.animationXAttention
+
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imvLogo.animationXAttention(Attention.ATTENTION_STAND_UP, 4000L)
+        runPostDelayed()
+
+    }
+
+    private fun runPostDelayed() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            goMainActivity()
+        }, 3000)
+    }
+
+    private fun goMainActivity(){
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
+    }
+}
